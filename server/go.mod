18528304c9d8d0434617c176a649ca6f4503662f
linzhengen <linzhengen@yahoo.co.jp>
2020-01-13 22:08:02 +0900

    fix server

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,7 @@ module github.com/linzhengen/grpc-lb-with-envoy/server
 go 1.13
 
 require (
+	github.com/linzhengen/grpc-lb-with-envoy/pb v0.0.0-20200113145020-81cd905a2e6a
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
-	google.golang.org/grpc v1.26.0 // indirect
+	google.golang.org/grpc v1.26.0
 )
---
+++ server/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+
+	"github.com/linzhengen/grpc-lb-with-envoy/pb"
+	"google.golang.org/grpc/metadata"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct{}
+
+// Show the hand
+func (s *server) Show(ctx context.Context, in *pb.JankenRequest) (*pb.JankenResponse, error) {
+	log.Printf("Handling JankenRequest request [%v] with context %v", in, ctx)
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Printf("Unable to get hostname %v", err)
+		hostname = ""
+	}
+	grpc.SendHeader(ctx, metadata.Pairs("hostname", hostname))
+	c := choice(pb.Choice_name)
+	log.Println(c)
+	return &pb.JankenResponse{
+		My:    in.My,
+		Your:  c,
+		IsWin: isWin(in.My, c),
+	}, nil
+}
+
+func choice(m map[int32]string) pb.Choice {
+	return pb.Choice(int32(rand.Intn(len(m))))
+}
+
+func isWin(o, t pb.Choice) bool {
+	return (o == pb.Choice_ROCK && t == pb.Choice_PAPER) || (o == pb.Choice_PAPER && t == pb.Choice_SCISSORS) || (o == pb.Choice_SCISSORS && t == pb.Choice_ROCK)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterJankenServer(grpcServer, &server{})
+	grpc_health_v1.RegisterHealthServer(grpcServer, &health.Server{})
+	reflection.Register(grpcServer)
+	log.Printf("Listening for Janken on port %s", port)
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
